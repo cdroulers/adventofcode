e0c3b511e8240a96d526dc0ab99e60dc4f90ec27
Christian Droulers <droulers.christian@gmail.com>
2024-12-04 11:00:23 -0500

    Day 4 - Part 2 i r dum and can't read good

--- 2024/AdventOfCode2024/Day4/Day4Tests.cs
+++ 2024/AdventOfCode2024/Day4/Day4Tests.cs
@@ -5,7 +5,9 @@ namespace AdventOfCode2024.Day4;
 
 public class Day4Tests
 {
-    private static readonly string Xmas = "XMAS";
+    private static readonly string Xmas = "MMSS";
+    private static readonly Point[] Rotation = [new(-1, -1), new(-1, 1), new(1, 1), new(1, -1)];
+    
     public int XmasFinder(string input)
     {
         var lines = input
@@ -22,16 +24,14 @@ public class Day4Tests
             for (var colIdx = 0; colIdx < line.Length; colIdx++)
             {
                 var l = line[colIdx];
-                if (l == 'X')
+                if (l == 'A')
                 {
-                    count += CheckXmas(lines, 1, new(colIdx, lineIdx), new(1, 0))
-                        + CheckXmas(lines, 1, new(colIdx, lineIdx), new(0, 1))
-                        + CheckXmas(lines, 1, new(colIdx, lineIdx), new(-1, 0))
-                        + CheckXmas(lines, 1, new(colIdx, lineIdx), new(0, -1))
-                        + CheckXmas(lines, 1, new(colIdx, lineIdx), new(-1, -1))
-                        + CheckXmas(lines, 1, new(colIdx, lineIdx), new(1, 1))
-                        + CheckXmas(lines, 1, new(colIdx, lineIdx), new(-1, 1))
-                        + CheckXmas(lines, 1, new(colIdx, lineIdx), new(1, -1));
+                    var point = new Point(colIdx, lineIdx);
+                    count += CheckXmas(lines, 0, point, 0)
+                        + CheckXmas(lines, 0, point, 1)
+                        
+                        + CheckXmas(lines, 0, point, 2)
+                        + CheckXmas(lines, 0, point, 3);
                 }
             }
         }
@@ -39,8 +39,9 @@ public class Day4Tests
         return count;
     }
 
-    private int CheckXmas(char[][] input, int xmasIdx, Point pos, Point diff)
+    private int CheckXmas(char[][] input, int xmasIdx, Point pos, int rotationIdx)
     {
+        var diff = Rotation[rotationIdx];
         var nextPos = new Point(pos.X + diff.X, pos.Y + diff.Y);
         if (nextPos.Y < 0 
             || nextPos.Y >= input.Length
@@ -52,37 +53,50 @@ public class Day4Tests
 
         if (input[nextPos.Y][nextPos.X] == Xmas[xmasIdx])
         {
-            return xmasIdx >= Xmas.Length - 1 ? 1 : CheckXmas(input, xmasIdx + 1, nextPos, diff);
+            if (xmasIdx >= Xmas.Length - 1)
+            {
+                return 1;
+            }
+
+            var nextRotationIdx = rotationIdx >= Rotation.Length - 1 ? 0 : rotationIdx + 1;
+            return CheckXmas(input, xmasIdx + 1, pos, nextRotationIdx);
         }
 
         return 0;
     }
 
     [Theory]
-    [InlineData(@"XMAS", 1)]
+    [InlineData(@"
+M.S
+.A.
+M.S
+S.S
+.A.
+M.M
+M.M
+.A.
+S.S", 3)]
     [InlineData(@"XXAMAS", 0)]
     [InlineData(
         @"
-XMAS
-XMAS
-XMAS
-XMAS
-SAMX",
-        7
+M.S
+.A.
+M.S",
+        1
     )]
     [InlineData(
         @"
-MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX",
-        18
+.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........",
+        9
     )]
     public void XmasFinderTest(string input, int expected)
     {
