d0c801505e620d75715a2c0d4192f7d7e4c02fc1
Christian Droulers <droulers.christian@gmail.com>
2024-12-09 21:04:03 -0500

    Day 9 - Part 2

--- 2024/AdventOfCode2024/Day9/Day9Tests.cs
+++ 2024/AdventOfCode2024/Day9/Day9Tests.cs
@@ -27,21 +27,71 @@ public class Day9Tests
 
     public List<int?> CompactDisk(List<int?> input)
     {
-        for (var i = 0; i < input.Count; i++)
+        var emptyIdx = input.IndexOf(null);
+        for (var i = input.Count - 1; i >= 0; i--)
         {
-            if (input[i] != null)
+            if (input[i] == null)
             {
                 continue;
             }
 
+            var len = 0;
+            var end = i;
+            var c = input[i];
+            if (c == 0)
+            {
+                // 0 block can never be moved.
+                continue;
+            }
             do
             {
-                input[i] = input[^1];
-                input.RemoveAt(input.Count - 1);
-            } while (i < input.Count && input[i] == null);
+                len++;
+                i--;
+            } while (input[i] == c);
+
+            //Fix idx because of reasons.
+            i++;
+
+            var start = i;
+
+            var range = FindEmptyRange(input, emptyIdx, len);
+            if (range != null && range.Value.Start.Value < i)
+            {
+                // Replace empty
+                for (var ri = range.Value.Start.Value; ri <= range.Value.End.Value; ri++)
+                {
+                    input[ri] = c;
+                }
+                
+                // Remove moved block
+                for (var ri = start; ri <= end; ri++)
+                {
+                    input[ri] = null;
+                }
+            }
         }
 
         return input;
+    }
+
+    private Range? FindEmptyRange(List<int?> input, int startIdx, int length)
+    {
+        while (true)
+        {
+            var idx = input.IndexOf(null, startIdx);
+            if (idx == -1)
+            {
+                return null;
+            }
+
+            var range = input.Skip(idx).Take(length).ToList();
+            if (range.Count >= length && range.All(x => x == null))
+            {
+                return new Range(idx, idx + length - 1);
+            }
+
+            startIdx = idx + 1;
+        }
     }
 
     public long Checksum(List<int?> input)
@@ -66,8 +116,8 @@ public class Day9Tests
     }
 
     [Theory]
-    [InlineData("0..111....22222", "022111222")]
-    [InlineData("00...111...2...333.44.5555.6666.777.888899", "0099811188827773336446555566")]
+    [InlineData("0..111....22222", "0..111....22222")]
+    [InlineData("00...111...2...333.44.5555.6666.777.888899", "00992111777.44.333....5555.6666.....8888..")]
     public void CompactDiskTest(string input, string expected)
     {
         var parsed = input.ToCharArray().Select(x => x == '.' ? (int?)null : int.Parse(x.ToString())).ToList();
@@ -79,6 +129,7 @@ public class Day9Tests
     [Theory]
     [InlineData("022111222", 60)]
     [InlineData("0099811188827773336446555566", 1928)]
+    [InlineData("00992111777.44.333....5555.6666.....8888..", 2858)]
     public void ChecksumTest(string input, long expected)
     {
         var parsed = input.ToCharArray().Select(x => x == '.' ? (int?)null : int.Parse(x.ToString())).ToList();
@@ -93,6 +144,6 @@ public class Day9Tests
         var diskMap = DiskMapToId(contents);
         var compacted = CompactDisk(diskMap);
         var total = Checksum(compacted);
-        total.Should().Be(6346871685398L);
+        total.Should().Be(6373055193464L);
     }
 }
